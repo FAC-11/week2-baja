809b4ca05f24bf1e8bedb7ed4b411a8811b2a7d0
Bart Bucknill <bartbucknill@hotmail.com>
2017-07-05 15:16:23 +0100

    reformat first addtodo function test

--- test.js
+++ test.js
@@ -9,10 +9,10 @@ test('Example test', function(t) {
 test('leave the input argument todos unchanged', function(t) {
   var expected = [];
   var todo = {
-id: 0,
-description: 'smash avocados',
-done: true,
-};
+            id: 0,
+            description: 'smash avocados',
+            done: true,
+            };
   var actual = todoFunctions.addTodo([], todo );
   t.deepEqual(actual, expected, "array should not be mutated");
   t.end();
