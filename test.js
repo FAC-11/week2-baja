27e98bb96083936197f5e924bcec0268c5c18ea3
alinasolinas <azayneeva@gmail.com>
2017-07-05 14:40:07 +0100

    test array not mutated

--- test.js
+++ test.js
@@ -5,3 +5,15 @@ test('Example test', function(t) {
   t.pass();
   t.end();
 });
+
+test('leave the input argument todos unchanged', function(t) {
+  var expected = [];
+  var todo = {
+id: 0,
+description: 'smash avocados',
+done: true,
+};
+  var actual = todoFunctions.addTodo([], todo );
+  t.deepEqual(actual, expected, "array should not be mutated");
+  t.end();
+});
